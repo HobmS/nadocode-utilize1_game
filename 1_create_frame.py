150b1ebfff6701df128b60cdba1c7343c449ffd8
HobmS <88806220+HobmS@users.noreply.github.com>
2023-03-16 12:35:56 +0900

    Add files via upload

---
+++ 1_create_frame.py
@@ -0,0 +1,14 @@
+class FourCal:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    def add(self):
+        result = self.first + self.second
+        return result
+
+a= FourCal(1,2)
+print(a.add(2,3))
+
+        
+        
